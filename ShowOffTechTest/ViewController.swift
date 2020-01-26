0d5686c2ebfe706acaa8b1ac9d57c70681dff0c0
Omar Bassyouni <omarbassyouni991@gmail.com>
2020-01-26 15:39:55 +0100

    Initial Commit

---
+++ ShowOffTechTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShowOffTechTest
+//
+//  Created by Omar Bassyouni on 1/26/20.
+//  Copyright © 2020 Omar Bassyouni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
